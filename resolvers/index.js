00b6dc39d12aa0383de1ca84abc70c3dddb986f7
chicodevbr <chicodevbr@gmail.com>
2022-05-25 18:02:32 -0300

    docs: :memo: documentando o index resolver

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,3 +1,5 @@
+// aqui vamos chamar os resolvers. Exemplo: const resolvers = [User, Community, Friends]
+// cada squad pode criar um arquivo pra resolver na pasta resolvers. Exemplo: FriendsResolvers.js
 const User = require('./UserResolvers');
 
 const resolvers = [User];
