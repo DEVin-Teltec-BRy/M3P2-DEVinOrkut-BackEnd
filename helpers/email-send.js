8ef3da10b1963e7aac408b5b8a2e2bb1528816ad
Daniel Meireles <daniel@inovadora.com.br>
2022-05-26 14:40:03 -0300

    feat: :memo: alterando start para adicionar middlewares.

--- helpers/email-send.js
+++ helpers/email-send.js
@@ -7,7 +7,6 @@ const Email = require('email-templates');
 
 const transport = nodemailer.createTransport(configEmail)
 
-
 const email = new Email({
   message: {
     from: 'no-reply@orkut.com'
@@ -15,22 +14,25 @@ const email = new Email({
   send: true,
   transport,
 });
-const sendEmail = async (user, sender, templateName) => {
+/**
+ * Função para enviar email
+ * @param {Object} user É o usuario de destino do email dados minimos: email e nome.
+ * @param {Object} variables É o objeto que será passado para o template, as chaves deste objeto se tornaram variaveis dentro do template.
+ * @param {String} templateName É o nome da pasta que contém o template.
+ * @returns 
+ */
+const sendEmail = async (user, variables, templateName) => {
     try {
      return await email.send({
         template: templateName,
         message: {
           to: user.email
         },
-        locals: {
-          name: user.name,
-          senderName: sender.name,
-          link: `http://localhost:3000/invite/${sender.id}`
-        },
+        locals: {...variables, user},
         preview: false,
       })
     } catch (error) {
-        console.error("O erro é: ", error);
+        throw new Error("Erro no envio do email: "+ error.message);
     }
 }
 module.exports = sendEmail;
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
+const path = require('path');
+
 const { importSchema } = require('graphql-import');
 const { port } = require('./config/environment');
 const resolvers = require('./resolvers');
@@ -11,22 +14,33 @@ const { getUserId } = require('./helpers/functions');
 
 const schemaPath = './schemas/index.graphql';
 
-const server = new ApolloServer({
-    typeDefs: importSchema(schemaPath),
-    resolvers,
-    playground: true,
-    tracing: true,
-    context: ({ req }) => {
-        return {
-            db,
-            userId: req && req.headers.authorization ? getUserId(req) : null,
-        };
-    },
-    dataSources: () => ({
-        users: new Users(db.User),
-    }),
-});
+const startApolloServer = async () => {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs: importSchema(schemaPath),
+        resolvers,
+        playground: true,
+        tracing: true,
+        context: ({ req }) => {
+            return {
+                db,
+                userId: req && req.headers.authorization ? getUserId(req) : null,
+            };
+        },
+        dataSources: () => ({
+            users: new Users(db.User),
+        })
+    });
+    await server.start();
+    
+    server.applyMiddleware({ app });
+    
+    app.use(express.static(path.join(__dirname, 'public')));
 
-server.listen({ port }).then(({ url }) => {
-    console.log(`\u{1F680} Server running on ${url}`);
-});
+    await new Promise(resolve => app.listen({ port }, resolve));
+
+    console.log(`\u{1F680} Server ready at http://localhost:${port}${server.graphqlPath}`);
+    return { server, app };
+};
+
+startApolloServer();
