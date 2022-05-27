2663bd4194cc40eaf489e106105349b78ff9fc66
sabrinamaral <sabrinamaral@hotmail.com>
2022-05-26 21:01:12 -0300

    Criação Scalar Date

---
+++ resolvers/Scalar.js
@@ -0,0 +1,20 @@
+const { GraphQLScalarType } = require('graphql');
+
+module.exports = {
+    DateScalar: new GraphQLScalarType({
+        name: 'Date',
+        description: 'Date custom scalar type',
+        serialize(value) {
+            return value.getTime(); // Convert outgoing Date to integer for JSON
+        },
+        parseValue(value) {
+            return new Date(value); // Convert incoming integer to Date
+        },
+        parseLiteral(ast) {
+            if (ast.kind === Kind.INT) {
+                return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
+            }
+            return null; // Invalid hard-coded value (not an integer)
+        },
+    }),
+};
