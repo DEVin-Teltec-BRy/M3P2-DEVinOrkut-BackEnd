f1f3daa0f1c2a0717d730e81aed08e03e5a6c304
Lmsilvano <75850731+Lmsilvano@users.noreply.github.com>
2022-05-27 20:25:21 -0300

    feature concluída
    inseridos funcionalidade de buscar comundiade por ID e listagem de membros com  limitações de 20 membros por requisição, no esquema limit offset

--- Resolvers/CommunityResolver.js
+++ Resolvers/CommunityResolver.js
@@ -6,6 +6,7 @@ const communityResolvers = {
 
     Query: {
         communities: async () => Community.find(),
+        community: async (_, { id }, { dataSources: { communities }, userId }) => communities.findOneById(id),
     },
 
     Mutation: {
@@ -27,6 +28,33 @@ const communityResolvers = {
             }
         },
     },
+    Community: {
+        members: async (communityArg, { limit, offset }, { dataSources: { users }, userId }, info) => {
+            const newLimit = !Number(limit) || Number(limit) > 20 ? 20 : Number(limit);
+            const newOffset = !Number(offset) ? 0 : Number(offset);
+            console.log(newOffset, newLimit)
+            try {
+
+                // A modificação da variavel userId(inserindo ID de algum membro da comunidade) 
+                // deve ser feita para efeito de testes, pois a feature de login
+                // não foi implementada no projeto. Excluir esse comentário quando a feature de login for implementada. 
+                userId = '62915a0f5e0e3f1b54c8cd02';
+                if (!userId) {
+                    return [];
+                }
+                const memberOrNot = await communityArg.members.find(member => member.toString() === userId.toString());
+                if (memberOrNot) {
+                    const data = await users.findManyByIds(communityArg.members)
+                    const response = await data.slice(newOffset, newOffset + newLimit).reverse();
+                    return response;
+                } else {
+                    return [];
+                }
+            } catch (error) {
+                return error.message;
+            }
+        },
+    }
 };
 
 module.exports = communityResolvers;
