61c73db84bfdd6d5b8554dee2ab9398c50fb38d5
chicodevbr <chicodevbr@gmail.com>
2022-05-31 11:40:18 -0300

    refactor: :recycle: apagando códigos desnecessários

--- Controllers/uploadController.js
+++ Controllers/uploadController.js
@@ -17,36 +17,10 @@ const uploads = async (req, res) => {
             imageBase64: Buffer.from(encode_img, 'base64'),
         };
 
-        // const imgArray = files.map(file => {
-        //     const img = fs.readFileSync(file.path);
-
-        //     return (encode_image = img.toString('base64'));
-        // });
-
-        // const result = imgArray.map((src, index) => {
-        //     const finalImg = {
-        //         filename: files[index].originalname,
-        //         contentType: files[index].mimetype,
-        //         imageBase64: src,
-        //         user: userId,
-        //     };
-
-        //     return finalImg;
-        // });
-
-        //const newUpload = new Upload(final_img);
-
         const user = await User.findOneAndUpdate(
             { _id: userId },
             { $push: { profilePicture: finalImg } },
         );
-
-        // const session = await mongoose.startSession();
-        // session.startTransaction();
-        // await newUpload.save({ session: session });
-        // user.profilePicture.push(newUpload);
-        // await user.save({ session: session });
-        // await session.commitTransaction();
 
         res.status(201).send(`Uploaded Successfully...!`);
     } catch (error) {
