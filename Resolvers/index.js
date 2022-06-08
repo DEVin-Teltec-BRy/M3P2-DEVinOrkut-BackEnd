33506bdee91a4a0ac1fa0a676e93e780a70303c5
sabrinamaral <sabrinamaral@hotmail.com>
2022-06-07 23:08:47 -0300

    altera resolvers

--- Resolvers/index.js
+++ Resolvers/index.js
@@ -1,10 +1,11 @@
 // aqui vamos chamar os resolvers. Exemplo: const resolvers = [User, Community, Friends]
 // cada squad pode criar um arquivo pra resolver na pasta resolvers. Exemplo: FriendsResolvers.js
 const User = require('./UserResolvers');
+const Category = require('./CategoryResolver');
 const Community = require('./CommunityResolver');
 const Forum = require('./ForumResolvers');
 const Coment = require('./ComentResolver');
 
-const resolvers = [User, Community, Forum, Coment];
+const resolvers = [User, Community, Forum, Coment, Category];
 
 module.exports = resolvers;
