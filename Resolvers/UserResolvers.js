7d300eb1846e51181f3a393816a430a853a74141
matheusgzamboni <matheusgzamboni@gmail.com>
2022-06-10 16:29:42 -0300

    add: Criada resolver updateUser para atualização de info do usuário

--- Resolvers/UserResolvers.js
+++ Resolvers/UserResolvers.js
@@ -353,6 +353,29 @@ const userResolvers = {
                 return error;
             }
         },
+        updateUser: async (_, { input }) => {
+            console.log("Update user!")
+            console.log("Input: ", input)
+            try {
+                const {id, ...dados} = input
+                const user = await Users.findById(id);
+                if (!user) return 'Usuário  inexistente';
+
+                const newUserData = await Users.updateOne(
+                    { _id: user.id },
+                    { ...dados },
+                );
+
+                console.log(`newUserData`, newUserData)
+
+                if (newUserData.modifiedCount === 0)
+                    return 'Nenhum dado atualizado';
+
+                return 'Dados atualizados com sucesso';
+            } catch (error) {
+                return error;
+            }
+        },
     },
     User: typesOfUser,
 };
